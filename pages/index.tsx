b05965d899dbc0008fdf5389cc69e7d680e21c7b
Varun Sharma <62218708+ve-varun-sharma@users.noreply.github.com>
2025-01-28 18:16:25 -0800

    feat: fix build issue with unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Portal from "../components/graphics/portal";
 import { useLogin } from "@privy-io/react-auth";
 import { PrivyClient } from "@privy-io/server-auth";
 import { GetServerSideProps } from "next";
